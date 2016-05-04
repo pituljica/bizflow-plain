7a3280caa436b8a8a1329adcd4325d7c16da1d5c
David Sljukic <dsljukic@blueberryapps.com>
2016-05-04 20:39:36 +0200

    Remove hello and world components

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,15 +1,11 @@
 import Counter from '../counter';
-import Hello from '../components/hello';
 import React, { Component } from 'react';
-import World from '../components/world';
 
 export default class App extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
         <Counter />
-        <Hello />
-        <World />
       </div>
     );
   }
